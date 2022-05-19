0487edc76caed981a2a99974d49158599bc7e6a0
Djeison Selzlein <djeison.selzlein@gmail.com>
2022-05-18 22:31:04 -0300

    #38 Callback chaining. Chain geocode and forecast calls.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,21 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Philadelphia New York', (error, data) => {
-  console.log(error)
-  console.log(data)
-})
-
-forecast(44.154622, -75.708847, (error, data) => {
-  console.log(error)
-  console.log(data)
-})
+const location = process.argv[2];
+if (location) {
+  geocode(location, (error, geocodeData) => {
+    if (error) {
+      return console.log(error)
+    }
+  
+    forecast(geocodeData.latitude, geocodeData.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error)
+      }
+      console.log(geocodeData.location)
+      console.log(forecastData)
+    })
+  })
+} else {
+  console.log('Please input location')
+}
