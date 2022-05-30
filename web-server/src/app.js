6f5dc4e63349206e72885e6a1d7301b07379f4e8
Djeison Selzlein <djeison.selzlein@gmail.com>
2022-05-30 17:56:51 -0300

    #47 Dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,10 +4,28 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-  res.send('Hello express!')
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Djeison'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'Djeison'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    message: 'This is where you can find help.'
+  })
 })
 
 app.get('/weather', (req, res) => {
