6efa21e8bcead162611a126a4ed65f006699507c
Djeison Selzlein <djeison.selzlein@gmail.com>
2022-06-04 17:32:04 -0300

    #57 http requests with fetch.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,17 @@
 console.log('client side javascript loaded')
+
+fetch('http://puzzle.mead.io/puzzle').then(res => {
+  res.json().then(data => {
+    console.log(data)
+  })
+})
+
+fetch('http://localhost:3000/weather?address=boston').then(res => {
+  res.json().then(data => {
+    if (data.error) {
+      console.log(data.error)
+    } else {
+      console.log(data)
+    }
+  })
+})
