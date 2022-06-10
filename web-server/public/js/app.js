2312ec4397745324cb6bcef5cb61dd8c607d2b38
Djeison Selzlein <djeison.selzlein@gmail.com>
2022-06-09 22:16:13 -0300

    #58 Creating a search form.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,17 +1,21 @@
 console.log('client side javascript loaded')
 
-fetch('http://puzzle.mead.io/puzzle').then(res => {
-  res.json().then(data => {
-    console.log(data)
-  })
-})
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
 
-fetch('http://localhost:3000/weather?address=boston').then(res => {
-  res.json().then(data => {
-    if (data.error) {
-      console.log(data.error)
-    } else {
-      console.log(data)
-    }
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const location = search.value
+
+  fetch('http://localhost:3000/weather?address=' + location).then(res => {
+    res.json().then(data => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        console.log(data)
+      }
+    })
   })
+
 })
