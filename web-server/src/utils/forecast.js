e75e750ac52f0f790d75d2a96724918857a5f330
Djeison Selzlein <djeison.selzlein@gmail.com>
2022-06-27 21:33:32 -0300

    #68 new feature deployment, refactor forecast message.

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -11,12 +11,10 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location')
     } else {
-      callback(undefined, {
-        description: body.current.weather_descriptions[0],
-        temperature: body.current.temperature,
-        feelsLike: body.current.feelslike,
-        humidity: body.current.humidity
-      })
+      callback(undefined, 
+        body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like "
+          + body.current.feelslike + " degrees out. The humidity is " + body.current.humidity + "%."
+      )
     }
   })
 }
