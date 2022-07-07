e0e62133d6486e2d17e28391829320482305f417
Djeison Selzlein <djeison.selzlein@gmail.com>
2022-07-06 22:46:30 -0300

    #75 Connecting and inserting documents.

---
+++ task-manager/mongodb.js
@@ -0,0 +1,19 @@
+// CRUD
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionUrl = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
+  if (error) {
+    return console.log('Unable to connect to database!')
+  }
+  
+  const db = client.db(databaseName)
+  db.collection('users').insertOne({
+    name: 'Djeison',
+    age: 27
+  })
+})
