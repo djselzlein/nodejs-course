3000de07926906676cf1dbd6a52045149c1d391d
Djeison Selzlein <djeison.selzlein@gmail.com>
2022-07-06 23:28:20 -0300

    #75 Inserting many documents.

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -12,8 +12,49 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
   }
   
   const db = client.db(databaseName)
-  db.collection('users').insertOne({
-    name: 'Djeison',
-    age: 27
+  // db.collection('users').insertOne({
+  //   name: 'Djeison',
+  //   age: 27
+  // }, (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert user')
+  //   }
+  //   console.log(result)
+  // })
+
+  // db.collection('users').insertMany([
+  //   {
+  //     name: 'Jen',
+  //     age: 28
+  //   },
+  //   {
+  //     name: 'Gunther',
+  //     age: 29
+  //   }
+  // ], (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert documents')
+  //   }
+  //   console.log(result)
+  // })
+
+  db.collection('tasks').insertMany([
+    {
+      description: 'Read article',
+      completed: true
+    },
+    {
+      description: 'Hang out',
+      completed: false
+    },
+    {
+      description: 'Do nothing',
+      completed: false
+    }
+  ], (error, result) => {
+    if (error) {
+      return console.log('Unable to insert documents')
+    }
+    console.log(result)
   })
 })
