da96327bdf9583d98bc710ad3b1df35e5a0e81ea
Djeison Selzlein <djeison.selzlein@gmail.com>
2022-07-20 22:26:58 -0300

    #81 Deleting documents.

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -12,49 +12,20 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
   
   const db = client.db(databaseName)
 
-  // db.collection('users').findOne({ _id: new ObjectId('62d75df383f5acdbfaf8d527') }, (error, user) => {
-  //   if (error) {
-  //     return console.log('Unable to fetch')
-  //   }
-  //   console.log(user)
-  // })
-
-  // db.collection('users').find({ age: 27 }).toArray((error, users) => {
-  //   console.log(users)
-  // })
-
-  // db.collection('tasks').findOne({}, { sort: { _id: -1 }}, (error, task) => {
-  //   console.log('Last task ID:')
-  //   console.log(task)
-  // })
-
-  // db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-  //   console.log(tasks)
-  // })
-
-  // db.collection('users').updateOne({
-  //   _id: new ObjectId('62c63d5aa88039d98ae6f3c9')
-  // }, {
-  //   $inc: {
-  //     age: 1
-  //   }
+  // db.collection('users').deleteMany({
+  //   age: 27
   // }).then((result) => {
   //   console.log(result)
   // }).catch((error) => {
   //   console.log(error)
   // })
 
-  db.collection('tasks').updateMany({
-    completed: true
-  }, {
-    $set: {
-      completed: false
-    }
+  db.collection('tasks').deleteOne({
+    description: 'Hang out'
   }).then((result) => {
     console.log(result)
   }).catch((error) => {
     console.log(error)
   })
-
 })
 
