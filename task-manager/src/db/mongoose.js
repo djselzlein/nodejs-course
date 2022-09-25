3ca16624d2c0c12aa8415e0c31d7b09b7b5739bb
Djeison Selzlein <djeison.selzlein@gmail.com>
2022-09-25 18:08:44 -0300

    #86 data validation and sanitization part 2

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -30,36 +30,51 @@ const User = mongoose.model('User', {
         throw new Error('Age must be a positive number!')
       }
     }
-  }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password can not contain "password"!')
+      }
+    }
+  },
 })
 
-const me = new User({
-  name: '  Djeison  ',
-  email: 'DJ@EMAIL.COM'
-})
-
-me.save().then(() => {
-  console.log(me)
-}).catch((error) => {
-  console.log('Error!', error)
-})
-
-// const Task = mongoose.model('Task', {
-//   description: {
-//     type: String
-//   },
-//   completed: {
-//     type: Boolean
-//   }
+// const me = new User({
+//   name: '  Djeison  ',
+//   email: 'DJ@EMAIL.COM',
+//   password: 'abcdef12'
 // })
 
-// const study = new Task({
-//   description: 'Study today',
-//   completed: true
-// })
-
-// study.save().then(() => {
-//   console.log(study)
+// me.save().then(() => {
+//   console.log(me)
 // }).catch((error) => {
 //   console.log('Error!', error)
 // })
+
+const Task = mongoose.model('Task', {
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const study = new Task({
+  description: '      Study today',
+  completed: true
+})
+
+study.save().then(() => {
+  console.log(study)
+}).catch((error) => {
+  console.log('Error!', error)
+})
