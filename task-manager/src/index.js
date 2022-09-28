e7c5e65e99b83bbcb9e0ba615555b5e6e4bc6f79
Djeison Selzlein <djeison.selzlein@gmail.com>
2022-09-27 22:28:09 -0300

    #100 Resource deleting endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -63,6 +63,18 @@ app.patch('/users/:id', async (req, res) => {
   }
 })
 
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id)
+    if (!user) {
+      return res.status(404).send()
+    }
+    res.send(user)
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
 app.post('/tasks', async (req, res) => {
   try {
     const task = new Task(req.body)
@@ -115,6 +127,18 @@ app.patch('/tasks/:id', async (req, res) => {
     return res.send(task)
   } catch (e) {
     return res.status(400).send(e)
+  }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id)
+    if (!task) {
+      return res.status(404).send()
+    }
+    res.send(task)
+  } catch (e) {
+    res.status(500).send()
   }
 })
 
