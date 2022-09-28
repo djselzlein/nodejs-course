199b9705cbd92ef8bb581c4ada25b67bc12a012e
Djeison Selzlein <djeison.selzlein@gmail.com>
2022-09-27 22:20:15 -0300

    #99 resource updating endpoints part 2

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -98,6 +98,26 @@ app.get('/tasks/:id', async (req, res) => {
   }
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['description', 'completed']
+  const isOperationAllowed = updates.every((update) => allowedUpdates.includes(update))
+
+  if (!isOperationAllowed) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    if (!task) {
+      return res.status(404).send()
+    }
+    return res.send(task)
+  } catch (e) {
+    return res.status(400).send(e)
+  }
+})
+
 app.listen(port, () => {
   console.log('Server is up on port ' + port)
 })
