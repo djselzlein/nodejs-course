d0d9a7557a750e2baaa42b048ff57b538cb84acd
Djeison Selzlein <djeison.selzlein@gmail.com>
2022-09-28 23:22:34 -0300

    #107 Generating authentication tokens

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -6,8 +6,9 @@ const router = new express.Router()
 router.post('/users', async (req, res) => {
   try {
     const user = new User(req.body)
+    const token = await user.generateAuthToken()
     await user.save()
-    res.status(201).send(user)
+    res.status(201).send({ user, token })
   } catch (e) {
     res.status(400).send(e)
   }
@@ -16,7 +17,8 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
   try {
     const user = await User.findByCredentials(req.body.email, req.body.password)
-    res.send(user)
+    const token = await user.generateAuthToken()
+    res.send({ user, token })
   } catch (e) {
     res.status(400).send()
   }
