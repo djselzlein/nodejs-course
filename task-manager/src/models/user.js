dcb1b504c9546a15c98d3764021d8589bb87ac94
Djeison Selzlein <djeison.selzlein@gmail.com>
2022-09-28 23:02:35 -0300

    #105 Logging in users

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -41,12 +42,27 @@ const userSchema = new mongoose.Schema({
   },
 })
 
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email })
+
+  if (!user) {
+    throw new Error('Unable to login')
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch) {
+    throw new Error('Unable to login')
+  }
+
+  return user
+}
+
+// Hash the plain text before saving
 userSchema.pre('save', async function (next) {
   const user = this;
 
-  console.log('pre save')
   if (user.isModified("password")) {
-    console.log('hashing password')
     user.password = await bcrypt.hash(user.password, 8);
   }
 
