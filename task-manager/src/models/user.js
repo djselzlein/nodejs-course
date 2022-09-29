0ca6937e88d014017329c5881f0c1390d46a76ef
Djeison Selzlein <djeison.selzlein@gmail.com>
2022-09-28 22:41:30 -0300

    #104 Securely storing passwords part 2

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -39,5 +40,19 @@ const User = mongoose.model('User', {
     }
   },
 })
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  console.log('pre save')
+  if (user.isModified("password")) {
+    console.log('hashing password')
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
